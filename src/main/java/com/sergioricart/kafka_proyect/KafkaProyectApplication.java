642b75b2a40b30e3da90b86ac6a01a8af2350b79
sergioricart <sergiricartt@gmail.com>
2026-05-01 12:25:25 +0200

    initial commit

---
+++ src/main/java/com/sergioricart/kafka_proyect/KafkaProyectApplication.java
@@ -0,0 +1,13 @@
+package com.sergioricart.kafka_proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProyectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergioricart/kafka_proyect/KafkaProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergioricart.kafka_proyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
