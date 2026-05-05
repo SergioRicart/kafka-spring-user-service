16eca8a453c4ddbde5e3cc4e8cce6d54e1ced992
sergioricart <sergiricartt@gmail.com>
2026-05-05 23:52:46 +0200

    feat: add .env

--- src/main/java/com/sergioricart/kafka_project/user/domain/entiry/User.java
+++ src/main/java/com/sergioricart/kafka_project/user/domain/entiry/User.java
@@ -1,6 +1,5 @@
 package com.sergioricart.kafka_project.user.domain.entiry;
 
-import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,31 +7,17 @@ import java.time.Instant;
 
 @Data
 @Builder
-@Entity
-@Table(name = "users")
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
-
     private String firstName;
-
     private String lastName;
-
     private String email;
-
     private Role role;
-
-    @Transient
     private Instant timestamp;
-
     private String password;
-
     Instant createdAt;
-
     Instant updatedAt;
-
     Instant deletedAt;
 
 }
