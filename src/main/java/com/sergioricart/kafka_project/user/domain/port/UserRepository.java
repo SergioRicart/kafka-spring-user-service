6ac81d63f02784ade9fa795101313299b9cb7001
sergioricart <sergiricartt@gmail.com>
2026-05-06 18:46:42 +0200

    feat: persistencia creada

--- src/main/java/com/sergioricart/kafka_project/user/domain/port/UserRepository.java
+++ src/main/java/com/sergioricart/kafka_project/user/domain/port/UserRepository.java
@@ -1,6 +1,7 @@
 package com.sergioricart.kafka_project.user.domain.port;
 
 import com.sergioricart.kafka_project.user.domain.entiry.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
--- src/main/java/com/sergioricart/kafka_project/user/infrastructure/database/UserRepositoryImpl.java
+++ src/main/java/com/sergioricart/kafka_project/user/infrastructure/database/UserRepositoryImpl.java
@@ -6,8 +6,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -15,40 +13,34 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
 
-    private final List<UserEntity> userEntityList = new ArrayList<>();
-
     private final UserEntityMapper userEntityMapper;
+    private final UserRepositoryData userRepositoryData;
 
     @Override
     public User save(User user) {
+
         UserEntity userEntity = userEntityMapper.mapToUserEntity(user);
 
-        userEntityList.stream().forEach(entity -> {
-            log.info("Saving User: {}", entity);
-        });
-
-        userEntityList.add(userEntity);
+        userRepositoryData.save(userEntity);
 
         return user;
     }
 
     @Override
     public Optional<User> findById(String id) {
-        return userEntityList.stream().filter(
-                userEntity -> userEntity.getId()
-                        .equals(id))
-                .findFirst()
+
+        return userRepositoryData.findById(id)
                 .map(userEntityMapper::mapToUser);
+
     }
 
     @Override
     public void deleteById(String id) {
-        userEntityList.removeIf(userEntity -> userEntity.getId().equals(id));
+        userRepositoryData.deleteById(id);
     }
 
     @Override
     public boolean existByEmail(String email) {
-        return userEntityList.stream()
-                .anyMatch(userEntity -> userEntity.getEmail().equals(email));
+        return userRepositoryData.existsByEmail(email);
     }
 }
